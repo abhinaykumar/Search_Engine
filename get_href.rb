be74d3640f30c5d87ad9eacde402c956cae4cf83
abhinay302 <abhinay302@gmail.com>
2014-12-26 21:06:29 +0530

    Round number

--- get_href.rb
+++ get_href.rb
@@ -1,9 +1,20 @@
+# Write Python code that assigns to the 
+# variable url a string that is the value 
+# of the first URL that appears in a link 
+# tag in the string page.
+# Your code should print http://udacity.com
+# Make sure that if page were changed to
+# page = '<a href="http://udacity.com">Hello world</a>'
+# that your code still prints the same thing.
+
+# page = contents of a web page
+
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open('https://www.udacity.com'))
+page = Nokogiri::HTML(open('https://www.udacity.com'))
 
-links = doc.css('a')
+links = page.css('a')
 
 links.each do |link|
   puts link['href']
